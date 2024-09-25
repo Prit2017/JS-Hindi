521756da14e1e11de44b88bf4333cc69bb8f9d94
Prit Patel <pritpatel7050@gmail.com>
2024-09-25 14:07:53 -0400

    datatypes

---
+++ 01basics/02_dataTypes.js
@@ -0,0 +1,19 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3 + 3); //we are using nodejs, not browser
+console.log("Prit");
+let name = "hites";
+let age = 18;
+let isLoggedIn = false;
+
+//number => 2 to power of 53
+//bigint
+// string
+// boolean => true/false
+//null => standalone value
+//undefined =>
+//symbol => unique
+
+//Object
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
