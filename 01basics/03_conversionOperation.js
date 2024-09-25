8393598b8161406b1cca17394c6e72324ce61505
Prit Patel <pritpatel7050@gmail.com>
2024-09-25 15:38:59 -0400

    con &operation

--- 01basics/03_conversionOperation.js
+++ 01basics/03_conversionOperation.js
@@ -7,7 +7,7 @@ console.log(typeof score);
 let valueInNumber = Number(score);
 console.log(typeof valueInNumber);
 console.log(valueInNumber);
-*/
+
 //"33" =>33
 //"33abc" => NaN
 // true => 1; false => 0
@@ -19,3 +19,34 @@ console.log(booleanIsLoggedIn);
 let someNumber = 33;
 let stringNumber = String(someNumber);
 console.log(typeof stringNumber);
+
+let value = 3;
+let negValue = -value;
+
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2%3) // reminder
+
+let str1 = "hello ";
+let str2 = "het";
+
+let str3 = str1 + str2;
+console.log(str3);
+
+//string conversation
+/*
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+console.log(((3 + 4) * 5) % 3);
+*/
+
+console.log(true);
+
+let num1, num2, num3;
+num1 = num2 = num3 = 2 + 2;
+
+let gameCounter = 100;
+++gameCounter;
+console.log(gameCounter);
