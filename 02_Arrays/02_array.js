faa98d9637da3bd7c07e8771cdd551d74c6d26f5
Prit Patel <pritpatel7050@gmail.com>
2024-09-26 14:17:37 -0400

    array2

---
+++ 02_Arrays/02_array.js
@@ -0,0 +1,21 @@
+const marvel_heros = ["Thor", "Ironman", "Spiderman"];
+const DC_heros = ["Superman", "Batman", "Flash"];
+/*
+//marvel_heros.push(DC_heros);
+//console.log(marvel_heros);
+//console.log(marvel_heros[3][1]);
+const allHeros = marvel_heros.concat(DC_heros);
+console.log(allHeros);
+*/
+const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]];
+const real_another_array = another_array.flat(Infinity);
+console.log(real_another_array);
+
+console.log(Array.isArray("Hitesh"));
+console.log(Array.from("Hitesh"));
+console.log(Array.from({ name: "hitesh" })); //Interesting
+let score1 = 100;
+let score2 = 200;
+let score3 = 300;
+
+console.log(Array.of(score1, score2, score3));
