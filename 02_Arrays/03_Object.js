dbf35c96503e71c217fb775a6619bc4e3850eae1
Prit Patel <pritpatel7050@gmail.com>
2024-09-26 14:29:05 -0400

    obj1

---
+++ 02_Arrays/03_Object.js
@@ -0,0 +1,15 @@
+//Singleton
+
+//Object literals
+const JsUser = {
+	name: "John",
+	"full name": "John Patel",
+	age: 18,
+	location: "Jaipur",
+	email: "hitesh1@gmail.com",
+	isLoggedIn: false,
+	lastLoginDays: ["Monday", "Saturday"],
+};
+
+console.log(JsUser.email);
+console.log(JsUser["email"]);
