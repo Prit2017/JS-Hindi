4ac859a74682875efa6406f53017c632f5e4c7ca
Prit Patel <pritpatel7050@gmail.com>
2024-09-30 16:15:08 -0400

    02/03

--- 02_Arrays/03_Object.js
+++ 02_Arrays/03_Object.js
@@ -11,7 +11,23 @@ const JsUser = {
 	lastLoginDays: ["Monday", "Saturday"],
 };
 
-console.log(JsUser.email);
-console.log(JsUser["email"]);
+// console.log(JsUser.email);
+// console.log(JsUser["email"]);
 
-console.log(JsUser.email);
+// console.log(JsUser.email);
+
+JsUser.email = "pritp@gmail.com";
+//Object.freeze(JsUser);
+JsUser.email = "prit@ms.com";
+//console.log(JsUser);
+
+JsUser.greeting = function () {
+	console.log("Hello JS User");
+};
+
+JsUser.greetingTwo = function () {
+	console.log(`Hello JS User, ${this.name}`);
+};
+
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
